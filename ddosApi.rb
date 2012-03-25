283943b7a0536f3f5a0e9740cd04694f4478ced8
Ben Jones <benlikescomputers@gmail.com>
2012-03-25 11:33:15 -0400

    Initial Thread Commit

--- ddosApi.rb
+++ ddosApi.rb
@@ -19,26 +19,30 @@ class DDOS_API
 
   #main ddos functions
   public
+  
+  #use this alias to point to the object on the server that is processing data
+  @fail_object
 
   #sockets to send and receive data and failover the server
-  @@failover_connnect
-  @@send_connections
-  @@receive_connections
+  @failover_connnect
+  @send_connections
+  @receive_connection
 
-  #this class variable stores the local ip address
-  @@local_ip
+  #this class variable stores the local ip 
+  @local_ip
+  @local_port
 
   #this timestamp records the last time that data was received
-  @@last_data_received
+  @last_data_received
 
   #this thread ensures that we are not in a ddos scenario
-  @@timeout_thread
+  @timeout_thread
   
   #this constant stores the amount of time to wait for new data before failing over
-  @@failover_timeout
+  @failover_timeout
 
   #class variable to store the array of backups
-  @@backups
+  @backups
 
   #use ints to store the type of connection
   SEND = 1
@@ -47,25 +51,32 @@ class DDOS_API
 
   #here are some basic accessor methods
   def get_failover_connection
-    @@failover_connect
+    @failover_connect
   end
-  def get_failover_timeout
-    @@failover_timeout
+  def get_failover_
+    @failover_timeout
   end
   
   
   #these functions are the actual meat of the API
+  
+  #use the constructor to initialize attributes that dont require processing
+  def initialize(failover_timeout, local_ip, local_port, fail_object)
+    #set the failover timeout to failover_timeout
+    @failover_timeout = failover_timeout
+
+    #set the incoming connection information
+    @local_ip = local_ip
+    @local_port = local_port
+
+    #set @fail_object up as a reference to the service's object
+    @fail_object
+  end
 
   #config_api: will read in the ip address of the next server to failover to and
   #store the socket in failover_connect
   #Note: this method should be called before using the rest of the API
-  def config_api(failover_filename="failover.txt", failover_timeout)
-    #set the failover timeout to failover_timeout
-    @@failover_timeout = failover_timeout
-
-    #start with a private method to get my local ip address
-    @@local_ip = get_local_ip
-
+  def config_api(failover_filename="failover.txt")
     #then read in the failover data from a file
     failover = File.open(failover_filename,"r")
 
@@ -89,7 +100,7 @@ class DDOS_API
       #if the last ip read was the ip and of this server, then select the next port
       #and ip. If this is the end of the file, then rewind to the beginning and use
       #the first connection listed in the text document
-      if(ip == @@local_ip || ip == 'localhost') then
+      if(ip == @local_ip) then
           line = failover.gets
           if(line == nil) then
               failover.rewind
@@ -114,10 +125,10 @@ class DDOS_API
     failover.close
 
     #now set up a thread to determine if a failover is needed
-    @@timeout_thread = Thread.new(Time.now) {|time| ddos_check(time) }
+    @timeout_thread = Thread.new(Time.now) {|time| ddos_check(time) }
     
     #set/ return the value of the failover connection
-    @@failover_connect = ip, port
+    @failover_connect = ip, port
   end
     
     
@@ -127,27 +138,27 @@ class DDOS_API
   #calling function
   def send_data(ip_address, port_num, *proc_data)
     connection = TCPSocket.new(ip_address, port_num)
-    connect_index = @@send_connections.length
-    @@send_connections << connection
+    connect_index = @send_connections.length
+    @send_connections << connection
     self.send_data(connect_index, proc_data)
   end
   #this function just looks up the connection info via its connectionIndex and then
   #sends the data
   def send_data(connect_index, *proc_data)
     #if this server has been "killed", then dont accept any more connections
-    return nil unless @@kill
+    return nil unless @kill
 
     #I have included some basic error handling here
     begin
       raise ArgumentError, "No argument" if connect_index.nil?
-      raise ArgumentError, "Invalid Connection index" if (connect_index < 0 || connect_index > @@send_connections.length)
+      raise ArgumentError, "Invalid Connection index" if (connect_index < 0 || connect_index > @send_connections.length)
       rescue => err
         puts err
       return connect_index
     end
     
     #now just send the data
-    @@send_connections[connect_index].puts proc_data
+    @send_connections[connect_index].puts proc_data
     
     #and finally return connect_index
     return connect_index
@@ -159,7 +170,7 @@ class DDOS_API
     #if this server has been "killed", then dont accept any more connections
     return nil unless @@kill
 
-    connection = @@receive_connections[connect_index]
+    connection = @receive_connections[connect_index]
     data = connection.gets
     #if the data triggers a kill, then call the kill function 
     self.kill if (0 == (/kill/ =~ data))
@@ -183,13 +194,13 @@ class DDOS_API
   #backups. If it is newer, it will replace the older backup and if not, it will
   #keep the old backup
   def receive_backup(connect_index)
-    connection = @@receive_connections[connect_index]
+    connection = @receive_connection[connect_index]
     time, backup_data = connection.gets.split(": ")
     
     #if this backup is newer, then use it
-    if(@@backups[connect_index][0] < time) then
-        @@backups[connect_index][0] = time
-        @@backups[connect_index][1] = backup_data
+    if(@backups[connect_index][0] < time) then
+        @backups[connect_index][0] = time
+        @backups[connect_index][1] = backup_data
     end
     #otherwise, just return true
     return true
@@ -217,7 +228,7 @@ class DDOS_API
   def old_ddos_check(timestamp)
     begin
       #wait for preset length of time
-      timeout(@@failover_timeout) do
+      timeout(@failover_timeout) do
         time = 1
         while true 
           puts "#{time}\n"
@@ -228,7 +239,7 @@ class DDOS_API
       end
       rescue TimeoutError
         return if timestamp < @@last_data_received
-        length = @@receive_connections.length -1
+        length = @receive_connection.length -1
         #now make sure that no one has
         0.upto(length) do |i|
            break if data = @@receive_connections[i].gets
